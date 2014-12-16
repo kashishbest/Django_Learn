1a07c06fe67650609500deb949dbb05c975b52fa
Kashish Singhal <singhal2.kashish@gmail.com>
2014-12-16 23:54:20 +0530

    Tutorial Page 1,

---
+++ mysite/polls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from polls.models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# Register your models here.
